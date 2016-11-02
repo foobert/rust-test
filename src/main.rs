687a895c75a99bf952fffce18081f1508485df68
foobert <0x002a@gmail.com>
2016-11-02 22:43:23 +0100

    Subscribe to topic and print debug on received message

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 extern crate libc;
-use libc::size_t;
 use std::ffi::CString;
 
 pub enum Mosquitto {}
+
+#[repr(C)]
+pub struct MosquittoMessage {
+    mid: u32,
+    topic: *mut libc::c_char,
+    payload: *mut libc::c_void,
+    payloadlen: u32,
+    qos: u32,
+    retain: bool,
+}
 
 #[link(name = "mosquitto")]
 extern {
@@ -12,6 +21,20 @@ extern {
     fn mosquitto_new(id: *const libc::c_char, clean_session: bool, arg: *mut libc::c_void) -> *const Mosquitto;
     fn mosquitto_destroy(mosq: *const Mosquitto);
     fn mosquitto_connect(mosq: *const Mosquitto, host: *const libc::c_char, port: u32, keepalive: u32) -> u32;
+    //fn mosquitto_loop(mosq: *const Mosquitto, timeout: u32, max_packets: u32) -> u32;
+    fn mosquitto_loop_forever(mosq: *const Mosquitto, timeout: u32, max_packets: u32) -> u32;
+    fn mosquitto_subscribe(mosq: *const Mosquitto, mid: *const u32, sub: *const libc::c_char, qos: u32) -> u32;
+    fn mosquitto_message_callback_set(mosq: *const Mosquitto, on_message: extern fn(*const Mosquitto, *const libc::c_void, *mut MosquittoMessage));
+}
+
+extern fn on_message(mosq: *const Mosquitto, obj: *const libc::c_void, message: *mut MosquittoMessage) {
+    println!("on_message");
+    unsafe {
+        let ref m = *message;
+        let mut t = m.topic;
+        let topic = CString::from_raw(t).into_string().expect("unable to load topic");
+        println!("topic: {}", topic);
+    }
 }
 
 fn main() {
@@ -22,6 +45,8 @@ fn main() {
     let id = "foo";
     let host = "test.mosquitto.org";
     let port = 1883;
+    let topic = "footest";
+    let mid = 0;
 
     unsafe {
         mosquitto_lib_init();
@@ -31,9 +56,15 @@ fn main() {
         let host: *const libc::c_char = CString::new(host).unwrap().as_ptr();
         let mosq = mosquitto_new(id, true, std::ptr::null_mut());
 
+        mosquitto_message_callback_set(mosq, on_message);
+
         let res = mosquitto_connect(mosq, host, port, 30);
         println!("connect: {}", res);
 
+        let topic: *const libc::c_char = CString::new(topic).unwrap().as_ptr();
+        mosquitto_subscribe(mosq, &mid, topic, 1);
+
+        mosquitto_loop_forever(mosq, 1000, 1);
 
         mosquitto_destroy(mosq);
         mosquitto_lib_cleanup();
